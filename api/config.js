7f11d1f479a05041c3f617ed7deb13d48ac660de
rhammer35 <rhammer35@icloud.com>
2021-05-18 16:49:14 -0400

    wrote user access functions required in users-model.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'wherehasthesecretgone',
   API_SECRET: process.env.API_SECRET || 'fillintheapisecret',
   PORT: process.env.PORT || 5000
 }
